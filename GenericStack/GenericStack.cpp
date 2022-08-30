70addbc41cabbbd3406d2f42967a27fd9b35163e
kksmash123 <kksmashes@gmail.com>
2022-08-30 10:31:32 +0530

    basic stack using push and pop methods

--- GenericStack/GenericStack.cpp
+++ GenericStack/GenericStack.cpp
@@ -1,20 +1,64 @@
-// GenericStack.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//Generic stack - here stack is implemented using array and linked list with template for reusability
 #include <iostream>
+using namespace std;
+
+
+template <class T>
+class stack
+{
+    T st[100];
+    int top;
+public:
+    stack();
+    void push(T x);
+    T pop();
+
+};
+
+//constructor
+
+template <class T>
+stack<T> ::stack()
+{
+    top = -1;
+}
+
+
+//push
+template <class T> 
+void stack<T>:: push(T x)
+{
+    st[++top] = x;
+
+}
+
+
+//pop
+template <class T>
+T stack<T>:: pop()
+{
+    return st[top--];
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    stack<int> int_stack;
+    stack<char> char_stack;
+    stack<string> string_stack;
+
+    int_stack.push(20);
+    char_stack.push('a');
+    string_stack.push("keerthana");
+
+    cout << int_stack.pop() << endl;
+    cout << char_stack.pop() << endl;
+    cout << string_stack.pop() << endl;
+
+
+
+    return 0;
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
